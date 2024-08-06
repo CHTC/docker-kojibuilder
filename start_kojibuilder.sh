57d40e70f678fbc7cfef719a90fea81f4196bda6
Matyas Selmeci <matyas@cs.wisc.edu>
2024-08-06 17:41:35 -0500

    Pull the default image from Harbor

--- start_kojibuilder.sh
+++ start_kojibuilder.sh
@@ -28,7 +28,7 @@ __TLDR__
 
 DEFAULT_CERT=$PWD/kojid.pem
 DEFAULT_ENV_FILE=$PWD/kojibuilder.cfg
-DEFAULT_IMAGE=osgpreview/koji-builder:testing-arm
+DEFAULT_IMAGE=hub.opensciencegrid.org/osgpreview/koji-builder:testing-arm
 DEFAULT_SITE_DEFAULTS=$PWD/mock-site-defaults.cfg
 
 
